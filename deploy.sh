5b79f31168f8a4f160880715bdfffd08c2c8bba7
seanscofield <seanvscofield@gmail.com>
2016-01-01 23:30:13 -0800

    small changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ echo "<script src='js/$OUTPUT_JS_FILE'></script>" | cat - $HTML_FILE > /tmp/out 
 git rm -f --cached js/*.js
 git add js/$OUTPUT_JS_FILE
 git add $HTML_FILE
-git commit -m $COMMIT_MSG
+git commit -m "$COMMIT_MSG"
 git push --force origin gh-pages
 
 # cleanup files and restore index.html to its previous state
